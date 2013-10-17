1d5d507aa120537d5839cc4a6498da9ed2dfc473
Brian Scott Dobrovodsky <bsdvodsky@gmail.com>
2013-10-17 08:04:29 -0700

    Minor Updates to StepAndDirection

--- CircleBuffer.h
+++ CircleBuffer.h
@@ -8,86 +8,86 @@
 
 class CircleBuffer {
 public:
-	CircleBuffer() //int bufferSize = 32)
-	{
-		size = 33; //bufferSize+1;
-		head = 0;
-		tail = 0;
-		//contents = (Vector *) malloc (sizeof (Vector) * size);
-	}
-	
-	~CircleBuffer ()
-	{
-		//free(contents);
-		//contents = NULL;
-		size = 0;
-		head = 0;
-		tail = 0;
-	}
-	
-	int isFull ()
-	{
-		if((tail+1)%size == head){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	int isEmpty()
-	{
-		if(tail == head){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	int push (const Vector i)
-	{
-		if(!isFull()){
-			contents[tail] = i;
-			tail = (tail + 1) % size;
-			return 0;
-		}
-		return -1;
-	}
-	
-	Vector pop ()
-	{
-		Vector elem = contents[head];
-		head = (head + 1) % size;
-		return elem;
-	}
-	
-	Vector next ()
-	{
-		Vector elem = contents[current];
-		if(head+tail > size) {
-			current = (current + 1) % (size-head + tail);
-		}
-		else {
-			current = (current + 1) % (head-tail);
-		}
-		return elem;
-	}
-	
-	bool isAvailable(int i)
-	{
-		for(int j = 0; j <= i; j++){
-			if((tail+1)%size == head){
-				return false;
-			}
-		}
-		return true;
-	}
+    CircleBuffer() //int bufferSize = 32)
+    {
+        size = 33; //bufferSize+1;
+        head = 0;
+        tail = 0;
+        //contents = (Vector *) malloc (sizeof (Vector) * size);
+    }
 
-private:
+    ~CircleBuffer ()
+    {
+        //free(contents);
+        //contents = NULL;
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    int isFull ()
+    {
+        if((tail+1)%size == head){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    int isEmpty()
+    {
+        if(tail == head){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    int push (const Vector i)
+    {
+        if(!isFull()){
+            contents[tail] = i;
+            tail = (tail + 1) % size;
+            return 0;
+        }
+        return -1;
+    }
+
+    Vector pop ()
+    {
+        Vector elem = contents[head];
+        head = (head + 1) % size;
+        return elem;
+    }
+
+    Vector next ()
+    {
+        Vector elem = contents[current];
+        if(head+tail > size) {
+            current = (current + 1) % (size-head + tail);
+        }
+        else {
+            current = (current + 1) % (head-tail);
+        }
+        return elem;
+    }
+
+    bool isAvailable(int i)
+    {
+        for(int j = 0; j <= i; j++){
+            if((tail+1)%size == head){
+                return false;
+            }
+        }
+        return true;
+    }
 
     int size;
-	int current;
+
+private:
+    int current;
     int head;
     int tail;
     Vector contents[33];
--- StepAndDirection.h
+++ StepAndDirection.h
@@ -10,15 +10,7 @@
 #define array_base_type 0
 typedef bool us1;	// for some reason typedef for bool or boolean do not work
 
-const uint32_t delay_5us   = CORE_TICK_RATE/1000*5;
-const uint32_t delay_10us  = CORE_TICK_RATE/1000*10;
-const uint32_t delay_25us  = CORE_TICK_RATE/1000*25;
-const uint32_t delay_50us  = CORE_TICK_RATE/1000*50;
-const uint32_t delay_125us = CORE_TICK_RATE/1000*125;
-const uint32_t delay_250us = CORE_TICK_RATE/1000*250;
-const uint32_t delay_1ms   = CORE_TICK_RATE/1000*1000;
-const uint32_t delay_250ms = CORE_TICK_RATE/1000*250000;
-
+// todo: control direction polarity when creating vectors
 class StepAndDirection {
 public:
 
@@ -28,65 +20,47 @@ public:
         StepAndDirection::pin_direction = pin_direction;
         StepAndDirection::pin_enable = pin_enable;
         StepAndDirection::pin_sleep = pin_sleep;
+
+        pinMode(pin_step, OUTPUT);
+        pinMode(pin_direction, OUTPUT);
+        pinMode(pin_enable, OUTPUT);
+        pinMode(pin_sleep, OUTPUT);
+        digitalWrite(pin_sleep, HIGH);
+        digitalWrite(pin_enable, LOW);
+        digitalWrite(c3p4, HIGH); // 8x microstepping
+//        digitalWrite(c1p4, LOW); // 16x microstepping
+
         motor_position = 0;
-
-        pinMode(pin_step,OUTPUT);
-        pinMode(pin_direction,OUTPUT);
-        pinMode(pin_enable,OUTPUT);
-        pinMode(pin_sleep,OUTPUT);
-        digitalWrite(pin_sleep,HIGH);
-        digitalWrite(pin_enable,LOW);
-
-        interruptPeriod = delay_5us;
         flag = false;
-        topside = false;
     }
 
-    uint32_t interrupt(uint32_t currentTime) {
-        if(vector.steps<=0 & vector.currentSkip<=0 & flag) {
-            if(!buffer.isEmpty()){
-                Serial.println("Next");
+    StepAndDirection(us8 motor, us8 card) {
+        *this = StepAndDirection(motor, KardIO[card][0], KardIO[card][1], KardIO[card][2], KardIO[card][3]);
+    }
+
+    // todo: verify skip starts at 1
+    void interrupt() {
+        if(vector.steps <= 0 && vector.currentSkip <= 0 && flag) {
+            if(!buffer.isEmpty()) {
                 vector = buffer.pop();
-                Serial.print(vector.steps);
-                Serial.print(" ");
-                Serial.println(vector.skip);
-                if(buffer.isEmpty()){
-                    flag = false;
-                    Serial.println(buffer.isEmpty());
-                }
+                digitalWrite(pin_direction, vector.direction);
+            }
+            else {
+                flag = false;
             }
         }
         if(vector.steps > 0 || vector.currentSkip > 0) {
             if(vector.currentSkip > 0) {
                 vector.currentSkip--;
             }
-            else{ //if(!topside){
+            else {
                 vector.currentSkip = vector.skip;
-
-                // step
-                //us1 temp = digitalRead(pin_step);
                 digitalWrite(pin_step, 1);
-//				topside = true;
-                us1 temp = digitalRead(pin_step);
                 motor_position++;
-                //digitalWrite(pin_step, 0);
-                if(temp) {
-                    vector.steps--;
-                }
+                vector.steps--;
                 digitalWrite(pin_step, 0);
-
-                //digitalWrite(pin_step, 0);
-                //motor_position[motor]++;
-                //digitalWrite(pin_step, 1);
             }
-//			else
-//			{
-//				digitalWrite(pin_step, 0);
-//				topside = false;
-//			}
         }
-
-        return (currentTime + interruptPeriod);
     }
 
     void command(TokenParser &parser)
@@ -94,51 +68,55 @@ public:
         if(parser.startsWith("stp?")) {
             parser.save();
             parser.advanceTail(3);
-            if( motor != parser.toVariant().toInt() - array_base_type) {
+            if(motor != parser.toVariant().toInt() - array_base_type) {
                 parser.restore();
                 goto done;
             }
 
             parser.nextToken();
-            if(parser.compare("vector")) {
-                vector.direction = 0;
-                vector.currentSkip = 0;
 
-                parser.nextToken();
-                vector.steps = parser.toVariant().toInt();
-
-                parser.nextToken();
-                vector.skip = parser.toVariant().toInt();
-
-                Serial.println("OK Vector");
-            }
             if(parser.compare("lock")) {
                 flag = false;
                 Serial.println("OK Lock");
             }
-            if(parser.compare("specific")) {
-                parser.nextToken();
-                int amount = parser.toVariant().toInt();
-                for(int i = 0; i < amount; i++){
-                    Vector temp;
-                    parser.nextToken();
-                    temp.steps = parser.toVariant().toInt();
 
-                    parser.nextToken();
-                    temp.skip = parser.toVariant().toInt();
+            if(parser.compare("unlock")) {
+                flag = true;
+                Serial.println("OK Unlock");
+            }
+
+            if(parser.compare("pairs")) {
+                for(int i = 0; i < buffer.size; i++) {
+                    if(!parser.nextToken()) {
+                        break;
+                    }
+                    String token = parser.toString();
+                    Serial.println(String(i, DEC) + ": " + token);
+
+                    int index = token.indexOf(",");
+
+                    Vector temp;
+                    temp.steps = token.substring(0, index).toInt();
+                    temp.skip = token.substring(++index).toInt();
                     buffer.push(temp);
                 }
-                Serial.println("OK Specific");
+                flag = true;
+                Serial.println("OK Pairs");
             }
+
             if(parser.compare("create")) {
                 parser.nextToken();
                 int start = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int stop = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int numsteps = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int stepheight = parser.toVariant().toInt();
+
                 Serial.println((start - stop)/stepheight);
                 if((start - stop) > 0) {
                     for(int i = 0; i <= (start - stop)/stepheight; i++){
@@ -159,16 +137,22 @@ public:
                 //flag = true;
                 Serial.println("OK Create");
             }
+
             if(parser.compare("trapezoidal")) {
                 flag = false;
+
                 parser.nextToken();
                 int start = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int stop = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int numsteps = parser.toVariant().toInt();
+
                 parser.nextToken();
                 int stepheight = parser.toVariant().toInt();
+
                 Serial.println((start - stop)/stepheight);
                 if((start - stop) > 0) {
                     for(int i = 0; i <= (start - stop)/stepheight; i++){
@@ -208,10 +192,6 @@ public:
                 flag = true;
                 Serial.println("OK Trapezoidal");
             }
-            if(parser.compare("unlock")) {
-                flag = true;
-                Serial.println("OK Unlock");
-            }
         }
         done:
             ;
@@ -225,11 +205,8 @@ private:
     us8 pin_sleep;
     us32 motor_position;
 
-    Variant timeBase;
-    uint32_t interruptPeriod;
-    Vector vector;
     bool flag;
-    bool topside;
+    Vector vector;
     CircleBuffer buffer;
 };
 
