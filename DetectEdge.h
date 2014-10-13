4f70a57191612fa18c94ba2f400410015e9bcce3
Michael Skoczen <mike@pontech.com>
2014-10-13 08:49:15 -0700

    added the de-bounce version of edge detect

--- DetectEdge.h
+++ DetectEdge.h
@@ -6,16 +6,20 @@
 
 class DetectEdge {
 public:
-    DetectEdge(us8 pin = 0, bool activeLow = true)
+    DetectEdge(us8 pin = 0, bool activeLow = true, us16 deBounce_ms = 0)
     {
         pPin = pin;
         pActiveLow = activeLow;
         pPreviousState = activeLow;
+        pPreviousStateBounce = activeLow;
         pRisingFlag = false;
         pFallingFlag = false;
 
         pTime = 0;
         pPreviousTime = 0;
+        pDebounceTime_ms = deBounce_ms;
+        pRisingWait = false;
+        pFallingWait = false;
     }
 
     void scan()
@@ -28,14 +32,34 @@ public:
         }
 
         if(input == 0 && pPreviousState != 0) {
-            pRisingFlag = false;
-            pFallingFlag = true;
+            pFallingWait = true;
+            pChangeTime = millis();
             stateChanged = true;
         }
         else if(input == 1 && pPreviousState != 1) {
-            pRisingFlag = true;
-            pFallingFlag = false;
+            pRisingWait = true;
+            pChangeTime = millis();
             stateChanged = true;
+        }
+        if(pRisingWait || pFallingWait )
+        {
+            if(CompareTime(pChangeTime, millis()) >= pDebounceTime_ms)
+            {
+                if(pFallingWait && pPreviousStateBounce && input == 0)
+                {
+                    pRisingFlag = false;
+                    pFallingFlag = true;
+                    pPreviousStateBounce = false;
+                } 
+                else if(pRisingWait && !pPreviousStateBounce && input == 1)
+                {
+                    pRisingFlag = true;
+                    pFallingFlag = false;
+                    pPreviousStateBounce = true;
+                }
+                pRisingWait = false;
+                pFallingWait = false;
+            }
         }
 
         if(stateChanged) {
@@ -102,10 +126,30 @@ private:
     us8 pPin;
     bool pActiveLow;
     bool pPreviousState;
+    bool pPreviousStateBounce;
     bool pRisingFlag;
     bool pFallingFlag;
     unsigned long pTime;
     unsigned long pPreviousTime;
+    us32 pChangeTime;
+    us16 pDebounceTime_ms;
+    bool pRisingWait;
+    bool pFallingWait;
+
+    us32 CompareTime(us32 start, us32 stop)
+    {
+        if(start > stop) //accout for rollover
+        {
+//            Serial.println((0xFFFFFFFF - start) + stop,DEC);
+//            Serial.println("boundry");
+            return (0xFFFFFFFF - start) + stop;
+        }
+        else
+        {
+//            Serial.println(stop - start,DEC);
+            return stop - start;
+        }
+    }
 };
 
 #endif // DETECTEDGE_H
